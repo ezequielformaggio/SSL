ed2d64cbc2cf70214f22d7b21d631489f2783dd7
Ezequiel Formaggio <60719564+ezequielformaggio@users.noreply.github.com>
2020-05-11 18:14:34 -0300

    Add files via upload

---
+++ 03-FasesErrores/hello2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int/*medio*/main(void){
+    int i = 42;
+    prontf("la respuesta es %d\n");
+}
---
+++ 03-FasesErrores/hello3.c
@@ -0,0 +1,6 @@
+
+int printf(const char *s, ...);
+
+int main(void){
+int i=42;
+pront("La respuesta es %d\n");
