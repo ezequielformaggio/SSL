d5789196c4bec352aa71c9b313ffdee7aef6c161
Ezequiel Formaggio <60719564+ezequielformaggio@users.noreply.github.com>
2020-06-01 18:01:29 -0300

    Add files via upload

---
+++ 04-Strings/StringTest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+Enunciado:
+Investigue y construya, en LENGUAJE C, la función que realiza cada operación solicitada:
+*/
+
+/* 
+* Ejercicio 21 *
+Construya un programa de testeo para cada 
+función del ejercicio anterior.
+*/
